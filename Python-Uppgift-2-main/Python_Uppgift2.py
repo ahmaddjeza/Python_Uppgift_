63d1e8223ef861e14afd09821e5e9bc9156bab99
ahmaddjeza <djezaahmad@gmail.com>
2025-03-27 19:25:47 +0100

    Läser in datan från excel filen.

--- Python-Uppgift-2-main/Python_Uppgift2.py
+++ Python-Uppgift-2-main/Python_Uppgift2.py
@@ -1,6 +1,15 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 import plotly.express as px
-import os
+
+#a) Börja med att läsa in filen riket2023_åk9_np.xlsx och sheets för de olika ämnena (engelska, matematik, svenska,
+# svenska som andraspråk). Ändra kolumnnamnen på respektive dataframe så du får något liknande som nedan.
+
+#Läser in datan från Excelfilen.
+df_Engelska = pd.read_excel('visualiseringar/riket2023_åk9_np.xlsx',skiprows=8, sheet_name="Engelska")
+df_Matematik = pd.read_excel('visualiseringar/riket2023_åk9_np.xlsx',skiprows=8, sheet_name="Matematik")
+df_Svenska = pd.read_excel('visualiseringar/riket2023_åk9_np.xlsx',skiprows=8, sheet_name="Svenska")
+df_Svenska_som_andraspråk = pd.read_excel('visualiseringar/riket2023_åk9_np.xlsx',skiprows=8, sheet_name="Svenska som andraspråk")
+
 
 
